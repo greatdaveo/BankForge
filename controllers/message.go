533f92f2fa8b1ae6b195cd0273423f7a67c86580
David Olowomeye <olowomeyedavid242@gmail.com>
2025-07-23 23:31:12 +0100

    Handled user confirmation replies with in memory session to track user chat records

--- controllers/message.go
+++ controllers/message.go
@@ -3,9 +3,14 @@ package controllers
 import (
 	"fmt"
 	"net/http"
+	"strings"
 
+	"github.com/greatdaveo/SendlyPay/models"
 	"github.com/greatdaveo/SendlyPay/services"
 )
+
+// In memory session
+var sessionStore = make(map[string]models.PaymentInfo)
 
 func HandleIncomingMessages(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
@@ -20,25 +25,63 @@ func HandleIncomingMessages(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// To extract whatsapp messages and sender
-	message := r.FormValue("Body")
+	message := strings.TrimSpace(strings.ToLower(r.FormValue("Body")))
 	from := r.FormValue("From")
+	fmt.Printf("📩 Message from %s: %s\n", from, message)
 
-	fmt.Printf("Message from %s: %s\n", from, message)
+	// To check if user has a pending payment in session
+	pending, hasSession := sessionStore[from]
 
-	// To Call AI to extract payment details
-	info, err := services.ExtractPaymentInfo(message)
+	// If yes confirm and process payment
+	if hasSession && message == "yes" {
+		fmt.Print("Confirmed payment: ", pending)
+		// To send Reply
+		reply := fmt.Sprintf("✅ Payment of £%.2f to %s is being processed.", pending.Amount, pending.RecipientName)
+		_ = services.SendWhatsAppMessage(from, reply)
+
+		// To clear session
+		delete(sessionStore, from)
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
+	// If No, cancel payment
+	if hasSession && message == "no" {
+		fmt.Printf("❌ Payment canceled by user: %+v\n", pending)
+
+		// To send cancellation reply
+		_ = services.SendWhatsAppMessage(from, "❌ Payment cancelled.")
+		delete(sessionStore, from)
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
+	// To Call AI to extract payment details as a new message, if otherwise
+	info, err := services.ExtractPaymentInfo(r.FormValue("Body"))
 	if err != nil {
-		fmt.Printf("Failed to extract payment info: %v\n", err)
+		fmt.Printf("❌ Failed to extract payment info: %v\n", err)
 		http.Error(w, "Failed to process message", http.StatusInternalServerError)
 		return
 	}
 
-	fmt.Println("Extracted Payment Info: -----------------------")
-	fmt.Printf("Action: %s\n", info.Action)
-	fmt.Printf("Amount: %.2f %s\n", info.Amount, info.Currency)
-	fmt.Printf("Recipient: %s\n", info.RecipientName)
-	fmt.Printf("Account Number: %s\n", info.AccountNumber)
-	fmt.Printf("Sort Code: %s\n", info.SortCode)
+	// To store in session for confirmation
+	sessionStore[from] = info
+
+	// To handle confirmation message
+	reply := fmt.Sprintf(
+		"✅ Got it. You want to send £%.2f to %s (Account: %s, Sort Code: %s).\nReply with 'Yes' to confirm or 'No' to cancel.",
+		info.Amount,
+		info.RecipientName,
+		info.AccountNumber,
+		info.SortCode,
+	)
+
+	// To send confirmation message from WhatsApp
+	if err := services.SendWhatsAppMessage(from, reply); err != nil {
+		fmt.Printf("❌ Failed to send confirmation: %v\n", err)
+		http.Error(w, "Failed to send message", http.StatusOK)
+		return
+	}
 
 	w.WriteHeader(http.StatusOK)
 }
--- services/twillio.go
+++ services/twillio.go
@@ -1,1 +1,50 @@
 package services
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+)
+
+func SendWhatsAppMessage(to string, message string) error {
+	accountSID := os.Getenv("TWILLIO_ACCOUNT_SID")
+	authToken := os.Getenv("TWILLIO_AUTH_TOKEN")
+	from := os.Getenv("TWILLIO_PHONE_NUMBER")
+
+	fmt.Println(".env: ", accountSID, authToken, from)
+
+	// To create the req payload
+	form := url.Values{}
+	form.Add("To", to)
+	form.Add("From", from)
+	form.Add("Body", message)
+
+	req, err := http.NewRequest(
+		"POST",
+		"https://api.twilio.com/2010-04-01/Accounts/"+accountSID+"/Messages.json",
+		strings.NewReader(form.Encode()),
+	)
+
+	if err != nil {
+		return err
+	}
+
+	req.SetBasicAuth(accountSID, authToken)
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	// To send the request
+	client := &http.Client{}
+	response, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode >= 200 && response.StatusCode < 300 {
+		return nil
+	}
+
+	return fmt.Errorf("failed to send message: status %d", response.StatusCode)
+}
