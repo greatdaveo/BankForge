4e0be7242f8ec91dfc34c68e92017a323856c58f
David Olowomeye <olowomeyedavid242@gmail.com>
2025-11-24 19:47:26 +0000

    Clean some logs by removing unnecessary details

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/greatdaveo/XChange/controllers"
+	"github.com/greatdaveo/XChange/rates"
+	"github.com/greatdaveo/XChange/routes"
+	"github.com/greatdaveo/XChange/services"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load(".env")
+	apiKey := os.Getenv("EXCHANGE_RATE_API_KEY")
+	currencyLayerKey := os.Getenv("CURRENCY_LAYER_API_KEY")
+
+	externalAPI := &rates.ExchangeRateAPI{
+		APIKey: apiKey,
+	}
+	frankfurterAPI := &rates.CurrencyLayerAPI{
+		APIKey: currencyLayerKey,
+	}
+
+	//  Pass both as RateProvider interface
+	rateService := services.NewRateService([]rates.RateProvider{
+		externalAPI,
+		frankfurterAPI,
+	})
+
+	r := routes.RegisterRoutes(controllers.ConvertCurrency(rateService))
+
+	log.Println("📌 Server running on :8080")
+	http.ListenAndServe(":8080", r)
+}
---
+++ rates/exchangerate.go
@@ -0,0 +1,53 @@
+package rates
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+type ExchangeRateAPI struct {
+	APIKey string
+}
+
+func (e *ExchangeRateAPI) FetchRate(from, to string) (float64, error) {
+	from = strings.ToUpper(from)
+	to = strings.ToUpper(to)
+
+	url := fmt.Sprintf("https://v6.exchangerate-api.com/v6/%s/latest/%s", e.APIKey, from)
+
+	response, err := http.Get(url)
+	if err != nil {
+		return 0, err
+	}
+	defer response.Body.Close()
+
+	var data struct {
+		Result          string             `json:"result"`
+		ConversionRates map[string]float64 `json:"conversion_rates"`
+	}
+	if err := json.NewDecoder(response.Body).Decode(&data); err != nil {
+		return 0, err
+	}
+	if data.Result != "success" {
+		return 0, errors.New("API response failed")
+	}
+
+	// fmt.Print("Response:", data)
+
+	// fmt.Println("Available rates:")
+	// for k := range data.ConversionRates {
+	// 	fmt.Print(k + " ")
+	// }
+
+	rate, ok := data.ConversionRates[to]
+	if !ok {
+		return 0, errors.New("target currency not found")
+	}
+
+	fmt.Println("📌 Final rate returned from API:", rate)
+
+	return rate, nil
+}
